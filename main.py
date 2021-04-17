1e3a3a12dbee5805cc55608ba6bc47a27737c438
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-04-17 13:27:44 +0400

    delete unuseful routs

--- main.py
+++ main.py
@@ -552,10 +552,10 @@ if __name__ == '__main__':
     db_session.global_init("db/users.db")
     port = int(os.environ.get("PORT", 8080))
     # app.run(host='0.0.0.0', port=port)
-    api.add_resource(restful_resources.InputsResource, '/api/user/<int:user_id>/inputs')
+    # api.add_resource(restful_resources.InputsResource, '/api/user/<int:user_id>/inputs')
     api.add_resource(restful_resources.ResultsResource, '/api/user/<int:user_id>/results')
 
-    api.add_resource(restful_resources.UpdateUser, '/api/user')
+    # api.add_resource(restful_resources.UpdateUser, '/api/user')
 
     # api.add_resource(restful_resources.I, '/api/user_inputs/<int:user_id>')
     serve(app, host='0.0.0.0', port=port)
